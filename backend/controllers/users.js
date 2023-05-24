57b13e478fa02af86b014df4bc79093e91bfe1eb
serp123616512 <serp123616512@yandex.ru>
2023-05-24 19:41:24 +0300

    fix: cookie

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -76,7 +76,7 @@ const signIn = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret-key', { expiresIn: '7d' });
-      return res.cookie('jwt', token, { maxAge: 3600000, httpOnly: true }).send({ message: 'Авторизация прошла успешно' });
+      return res.cookie('jwt', token, { maxAge: 3600000, httpOnly: true, sameSite: false }).send({ message: 'Авторизация прошла успешно' });
     })
     .catch(next);
 };
