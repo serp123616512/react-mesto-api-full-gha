0b9f98075076fab5ba5b06b875ae972df5bb865d
serp123616512 <serp123616512@yandex.ru>
2023-05-25 12:06:19 +0300

    fix: cookie @1.0.3

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -81,7 +81,6 @@ const signIn = (req, res, next) => {
           maxAge: 3600000,
           httpOnly: true,
           sameSite: 'none',
-          secure: true,
         })
         .send({ message: 'Авторизация прошла успешно' });
     })
